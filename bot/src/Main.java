8fb7475161718b46d195d4331675f2dc6c175e16
BuildTools <unconfigured@null.spigotmc.org>
2016-07-29 11:18:12 +0200

    Spam und voteend verändert

--- bot/src/Main.java
+++ bot/src/Main.java
@@ -390,7 +390,9 @@ public class Main
 							int anzahl_I = Integer.parseInt(anzahl_S);
 							
 							for(int i=0;i<anzahl_I;i++){
-								api.sendServerMessage(nachricht);
+								System.out.println("spam" + i);
+								int serverID = api.getServerIdByPort(12200);
+								api.sendServerMessage(serverID, nachricht);
 							}
 						}	
 					}
@@ -483,9 +485,10 @@ public class Main
 					{
 						vote_gewinner = vote_m4;
 					}
+					int serverID = api.getServerIdByPort(12200);
 					api.sendPrivateMessage(e.getInvokerId(), "vote gewinner: " + vote_gewinner + " mit: " + a_vote_gewinner + " Stimmen!");
-					api.sendServerMessage("vote ist beendet!");
-					api.sendServerMessage("vote gewinner: " + vote_gewinner + " mit: " + a_vote_gewinner + " Stimmen!");
+					api.sendServerMessage(serverID,"vote ist beendet!");
+					api.sendServerMessage(serverID,"vote gewinner: " + vote_gewinner + " mit: " + a_vote_gewinner + " Stimmen!");
 					a_vote_m1 = 0;
 					a_vote_m2 = 0;
 					a_vote_m3 = 0;
