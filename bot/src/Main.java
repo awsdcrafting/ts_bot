6dd8144d60349851f6e1b035137a2e469795a766
BuildTools <unconfigured@null.spigotmc.org>
2016-08-01 13:50:56 +0200

    fehler in warnung behoben

--- bot/src/Main.java
+++ bot/src/Main.java
@@ -554,7 +554,7 @@ public class Main
 						}
 					}
 					if(warnungExistiertNicht){
-						alWarnungen.add(name + anzahlWarnungen);
+						alWarnungen.add(name + " " + anzahlWarnungen);
 					}
 					warnungExistiertNicht = true;
 					io.github.awsdcrafting.WarnSystem.SchreibeWarnung(alWarnungen);
