89e5ec4e8a87c0d0bdbda00015fc64aa878501b2
awsdcrafting <scisneromam@t-online.de>
2017-01-30 15:28:17 +0100

    *Formatting

--- bot/src/io/github/awsdcrafting/commands/commands/Kick.java
+++ bot/src/io/github/awsdcrafting/commands/commands/Kick.java
@@ -11,14 +11,14 @@ public class Kick extends Command
 
 	public Kick()
 	{
-		
-		super("Kick", "Kicks the named client from the server", new String[]{"Kick"}, main.Main.commandManager.getModLevel());
+
+		super("Kick", "Kicks the named client from the server",
+				new String[]{"Kick"}, main.Main.commandManager.getModLevel());
 	}
 
 	@Override
 	public void execute(TS3Api api, TextMessageEvent e, String[] args)
 	{
-		// TODO Auto-generated method stub
 		if (args.length < 1)
 		{
 			help();
@@ -34,11 +34,13 @@ public class Kick extends Command
 			{
 				kickGrund = args[1];
 			}
-			try{
+			try
+			{
 				int clID = Integer.parseInt(clientName);
 				for (Client client : api.getClients())
 				{
-					System.out.println("apiClientName: " + client.getNickname());
+					System.out
+							.println("apiClientName: " + client.getNickname());
 					int apiClientID = client.getId();
 
 					if (apiClientID == clID)
@@ -48,10 +50,12 @@ public class Kick extends Command
 						api.sendChannelMessage(clientName + " wurde gekickt!");
 					}
 				}
-			}catch(NumberFormatException e1){
+			} catch (NumberFormatException e1)
+			{
 				for (Client client : api.getClients())
 				{
-					System.out.println("apiClientName: " + client.getNickname());
+					System.out
+							.println("apiClientName: " + client.getNickname());
 					String apiClientName = client.getNickname();
 
 					if (apiClientName.equals(clientName))
@@ -63,7 +67,7 @@ public class Kick extends Command
 					}
 				}
 			}
-			
+
 		}
 
 	}
