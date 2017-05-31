754916814fbe8fff4c071c6684c41acd8e4771a4
awsdcrafting <scisneromam@t-online.de>
2017-05-31 18:23:55 +0200

    Updated Config system still not finished

--- bot/src/io/github/awsdcrafting/commands/impl/Config.java
+++ bot/src/io/github/awsdcrafting/commands/impl/Config.java
@@ -4,6 +4,8 @@ import com.github.theholywaffle.teamspeak3.api.event.TextMessageEvent;
 import io.github.awsdcrafting.commands.Command;
 import main.Main;
 
+import java.util.ArrayList;
+
 public class Config extends Command
 {
 
@@ -18,6 +20,8 @@ public class Config extends Command
 		if (args.length < 1)
 		{
 			api.sendPrivateMessage(e.getInvokerId(), help());
+			api.sendPrivateMessage(e.getInvokerId(), "Syntax: <default/reload/setting name <new value>>");
+
 		} else
 		{
 			if (args[0].equalsIgnoreCase("set"))
@@ -25,12 +29,44 @@ public class Config extends Command
 				if (args.length < 2)
 				{
 					api.sendPrivateMessage(e.getInvokerId(), "Sets the config");
-					api.sendPrivateMessage(e.getInvokerId(), "default or a setting");
+					api.sendPrivateMessage(e.getInvokerId(), "Syntax: !config <default/reload/setting name <new value>>");
 				} else
 				{
-					if (args[1].equalsIgnoreCase("default"))
+					if (args.length == 3)
 					{
-						Main.configManager.setDefaultConfig();
+						if (args[1].equalsIgnoreCase("default"))
+						{
+							Main.configManager.setDefaultConfig();
+						} else
+						{
+
+							if (args[1].equalsIgnoreCase("reload"))
+							{
+								Main.configManager.load();
+								Main.setConfig();
+							} else
+							{
+								api.sendPrivateMessage(e.getInvokerId(), "Wrong Syntax!");
+								api.sendPrivateMessage(e.getInvokerId(), "!config <default/reload/setting_name <new_value>>");
+							}
+						}
+					} else
+					{
+						if (args.length == 4)
+						{
+							Main.configManager.load();
+							ArrayList<String> config = Main.configManager.getConfig();
+							String setting_Name = args[2];
+							String setting_Value = args[3];
+
+							for (int i = 0; i < config.size(); i++)
+							{
+								if(config.get(i).equalsIgnoreCase(setting_Name)){
+									config.set(i,setting_Name+"="+setting_Value);
+								}
+							}
+
+						}
 					}
 				}
 			}
